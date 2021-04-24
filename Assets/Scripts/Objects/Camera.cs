4ec5ea7e7ea7251ed7e841d265a00c7516be3bbd
NoobHereo <NoobHereo@users.noreply.github.com>
2021-04-24 11:38:18 +0200

    Basic Controls

---
+++ Assets/Scripts/Objects/Camera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace game.Objects
+{
+    public class Camera : MonoBehaviour
+    {
+        private Transform target;
+        private bool targetSet = false;
+        public Vector3 Offset;
+
+        public void SetTarget(Transform target)
+        {
+            if (target != null)
+            {
+                this.target = target;
+                targetSet = true;
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (targetSet)
+            {
+                transform.position = target.localPosition - Offset;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace game.Objects
+{
+    public class Player : MonoBehaviour
+    {
+        private Rigidbody2D rb;
+        private game.Objects.Camera cam;
+        public float Speed = 100f;
+
+        private void Start()
+        {
+            configComponenets();
+        }
+
+        private void configComponenets()
+        {
+            gameObject.AddComponent<Rigidbody2D>();
+            rb = GetComponent<Rigidbody2D>();
+            rb.gravityScale = 0;
+            cam = UnityEngine.Camera.main.GetComponent<Camera>();
+            cam.SetTarget(transform);
+        }
+
+        private void Update()
+        {
+            var horizontal = Input.GetAxisRaw("Horizontal");
+            var vertical = Input.GetAxisRaw("Vertical");
+
+            rb.velocity = new Vector2(horizontal * Speed * Time.fixedDeltaTime, vertical * Speed * Time.fixedDeltaTime);
+        }
+    }
+}
