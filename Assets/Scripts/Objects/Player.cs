e2f7780a8b31c7cefe0d585c8cadde61c450d5be
NoobHereo <NoobHereo@users.noreply.github.com>
2021-04-24 12:02:59 +0200

    Animation

--- Assets/Scripts/Objects/Player.cs
+++ Assets/Scripts/Objects/Player.cs
@@ -7,6 +7,7 @@ namespace game.Objects
         private Rigidbody2D rb;
         private game.Objects.Camera cam;
         public float Speed = 100f;
+        private PlayerAnimator animator;
 
         private void Start()
         {
@@ -17,6 +18,7 @@ namespace game.Objects
         {
             gameObject.AddComponent<Rigidbody2D>();
             rb = GetComponent<Rigidbody2D>();
+            animator = GetComponent<PlayerAnimator>();
             rb.gravityScale = 0;
             cam = UnityEngine.Camera.main.GetComponent<Camera>();
             cam.SetTarget(transform);
@@ -25,7 +27,24 @@ namespace game.Objects
         private void Update()
         {
             var horizontal = Input.GetAxisRaw("Horizontal");
+            var horizontalAbs = Mathf.Abs(horizontal);
             var vertical = Input.GetAxisRaw("Vertical");
+            var verticalAbs = Mathf.Abs(vertical);
+
+            if (horizontalAbs < 0.1f && verticalAbs < 0.1f)
+            {
+                rb.velocity = Vector2.zero;
+                animator.UpdateSprite(SpriteState.Idle);
+            }
+
+            if (horizontalAbs > verticalAbs)
+            {
+                animator.UpdateSprite(horizontal > 0 ? SpriteState.Right : SpriteState.Left);
+            }
+            else
+            {
+                animator.UpdateSprite(vertical > 0 ? SpriteState.Up : SpriteState.Down);
+            }
 
             rb.velocity = new Vector2(horizontal * Speed * Time.fixedDeltaTime, vertical * Speed * Time.fixedDeltaTime);
         }
---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+
+namespace game
+{
+    public enum SpriteState
+    {
+        Idle,
+        Right,
+        Left,
+        Up,
+        Down
+    }
+
+    public class PlayerAnimator : MonoBehaviour
+    {
+        private SpriteRenderer renderer;
+
+        private SpriteState state = SpriteState.Down;
+
+        public Sprite[] MoveRight;
+        public Sprite[] MoveUp;
+        public Sprite[] MoveDown;
+
+        private int _rightLength;
+        private int _upLength;
+        private int _downLength;
+
+        private int animationCount;
+        private float timer = 0;
+        public float AnimationSpeed = 0.5f;
+
+
+        private void Start()
+        {
+            renderer = GetComponent<SpriteRenderer>();
+            _rightLength = MoveRight.Length;
+            _upLength = MoveUp.Length;
+            _downLength = MoveDown.Length;            
+        }
+
+        private void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer < AnimationSpeed)
+                return;
+            timer = 0;
+            Animate();
+            animationCount++;
+        }
+
+        public void UpdateSprite(SpriteState spriteState)
+        {
+            if (spriteState == state)
+                return;
+            animationCount = 0;
+            state = spriteState;
+            Animate();
+        }
+
+        private void Animate()
+        {
+            switch(state)
+            {
+                case SpriteState.Idle:
+                    renderer.sprite = MoveDown[0];
+                    renderer.flipX = false;
+                    break;
+
+                case SpriteState.Right:
+                    if (animationCount >= _rightLength)
+                        animationCount = 0;
+                    renderer.sprite = MoveRight[animationCount];
+                    renderer.flipX = false;
+                    break;
+
+                case SpriteState.Left:
+                    if (animationCount >= _rightLength)
+                        animationCount = 0;
+                    renderer.sprite = MoveRight[animationCount];
+                    renderer.flipX = true;
+                    break;
+
+                case SpriteState.Up:
+                    if (animationCount >= _upLength)
+                        animationCount = 0;
+                    renderer.sprite = MoveUp[animationCount];
+                    renderer.flipX = false;
+                    break;
+
+                case SpriteState.Down:
+                    if (animationCount >= _downLength)
+                        animationCount = 0;
+                    renderer.sprite = MoveDown[animationCount];
+                    renderer.flipX = false;
+                    break;
+            }
+        }
+
+    }
+}
